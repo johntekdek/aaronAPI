d775e41ea243db257557b5f37b85f28a74645ee9
Tekdek <john.tekdek@produban.co.uk>
2019-07-23 11:50:30 +0100

    added tasks

--- test_todoserver.py
+++ test_todoserver.py
@@ -3,14 +3,32 @@ import sys
 sys.path.append('aaronAPI')
 from todoserver import app
 import json
+app.testing =True
 
 def test_response():
     client = app.test_client()
     resp = client.get('/')    
     assert 200 == resp.status_code
+    assert b'Hello World !' == resp.data
     
 def test_empty_list_of_tasks():
     client = app.test_client()
     resp = client.get('/tasks/')    
     data = json.loads(resp.data.decode("utf-8"))
     assert data == []
+
+def test_create_a_task_and_get_its_details():
+    # arrange 
+    client = app.test_client()
+    resp = client.get("/tasks/")
+    data = json.loads(resp.data.decode("utf-8"))    
+    # action
+    new_task_data = {
+        "summary":"get milk",
+        "desc": "one gallon of milk"
+    }
+    resp = client.post("/tasks/", data = json.dumps(new_task_data),follow_redirects=True)
+    assert 201 == resp.status_code
+    data = json.loads(resp.data.decode("utf-8"))
+    assert 'id' in data
+
--- todoserver.py
+++ todoserver.py
@@ -1,15 +1,21 @@
 from flask import Flask,make_response
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return 'Hello'
+    return 'Hello World !'
 
-@app.route("/tasks/")
+@app.route("/tasks/", methods=["GET"])
 def get_all_tasks():
-    return make_response('[]',201)
+    return make_response("[]",200)
+
+@app.route("/tasks/", methods=["POST"])
+def create_task():
+    data = {"id":1}
+    return make_response(json.dumps(data),201)
 
 if __name__ == "__main__":
     app.run()
